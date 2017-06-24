4a52226fe6feaa1f41d90baae8ce51212b1c1bc7
PR1ME <sanya_fl@ukr.net>
2017-06-24 19:09:06 +0300

    test

--- views/mail/mailform.php
+++ views/mail/mailform.php
@@ -24,7 +24,7 @@ use yii\helpers\Html;
 
     <?php
 //debug($mail);
-    $form = ActiveForm::begin([
+    $mailform = ActiveForm::begin([
         'id' => 'mail-form',
         'options' => ['class' => 'form-horizontal'],
         'fieldConfig' => [
@@ -41,9 +41,9 @@ use yii\helpers\Html;
         ],
     ]);
     ?>
-    <?= $form->field($mail, 'recipient')->input('email', ['placeholder' => 'Введите email получателя']); ?>
-    <?= $form->field($mail, 'subject')->input('string',['placeholder' => 'Введите тему письма']); ?>
-    <?= $form->field($mail, 'message')->textarea(['rows' => 7, 'placeholder' => 'Введите текст сообщения']); ?>
+    <?= $mailform->field($mail, 'recipient')->input('email', ['placeholder' => 'Введите email получателя']); ?>
+    <?= $mailform->field($mail, 'subject')->input('string',['placeholder' => 'Введите тему письма']); ?>
+    <?= $mailform->field($mail, 'message')->textarea(['rows' => 7, 'placeholder' => 'Введите текст сообщения']); ?>
     <div class="form-group" align="right">
         <div>
             <?= Html::submitButton('Отправить', ['class' => 'btn btn-success']) ?>
@@ -52,54 +52,3 @@ use yii\helpers\Html;
 
     <?php ActiveForm::end() ?>
 </div>
-
-
-<!--<div class="col-lg-10 col-md-10 col-sm-10 col-xs-10 container mail-form">-->
-<!--	<form action="" method="post" class="form-horizontal">-->
-<!--		-->
-<!--		<div class="form-group mail-form__row">-->
-<!--			<div class="col-lg-2 col-md-3 col-sm-3 col-xs-3 mail-form__label">-->
-<!--				<label for="recipient" class="control-label">Получатель</label>-->
-<!--			</div>-->
-<!--			<div class="col-lg-7 col-md-7 col-sm-7 col-xs-7 mail-form__input-size">-->
-<!--				<input type="text" class="form-control" name="recipient" placeholder="Введите Email получателя">	-->
-<!--			</div>-->
-<!--			<div class="col-lg-3 col-md-2 col-sm-2 col-xs-2">-->
-<!--			</div>	-->
-<!--		</div>-->
-<!--		-->
-<!--		<div class="form-group mail-form__row">-->
-<!--			<div class="col-lg-2 col-md-3 col-sm-3 col-xs-3 mail-form__label">-->
-<!--				<label for="subject" class="control-label">Тема письма</label>-->
-<!--			</div>-->
-<!--			<div class="col-lg-7 col-md-7 col-sm-7 col-xs-7 mail-form__input-size">-->
-<!--				<input type="text" class="form-control" name="subject" placeholder="Введите тему письма">-->
-<!--			</div>-->
-<!--			<div class="col-lg-3 col-md-2 col-sm-2 col-xs-2">-->
-<!--			</div>	-->
-<!--		</div>-->
-<!--		-->
-<!--		<div class="form-group mail-form__row">-->
-<!--			<div class="col-lg-2 col-md-3 col-sm-3 col-xs-3 mail-form__label">-->
-<!--				<label for="message" class="control-label">Текст письма</label>-->
-<!--			</div>-->
-<!--			<div class="col-lg-7 col-md-7 col-sm-7 col-xs-7 mail-form__input-size">-->
-<!--				<textarea id="textfield" cols="30" rows="10" class="form-control" name="message" placeholder="Введите текст письма"></textarea> -->
-<!--			</div>-->
-<!--			<div class="col-lg-3 col-md-2 col-sm-2 col-xs-2">-->
-<!--			</div>	-->
-<!--		</div>-->
-<!--		-->
-<!--		<div class="mail-form__row">-->
-<!--			<div class="col-lg-2 col-md-3 col-sm-3 col-xs-3 mail-form__label">-->
-<!--			</div>-->
-<!--			<div class="col-lg-7 col-md-7 col-sm-7 col-xs-7 mail-form__input-size">-->
-<!--				<input type="submit" name="send" class="btn btn-success" value="Отправить">-->
-<!--				<input type="submit" name="cancel" class="btn btn-danger" value="Отмена">-->
-<!--			</div>-->
-<!--			<div class="col-lg-3 col-md-2 col-sm-2 col-xs-2">-->
-<!--			</div>-->
-<!--		</div>-->
-<!---->
-<!--	</form>-->
-<!--</div>-->
