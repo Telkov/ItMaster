82531f567bfc74afc1f88f7a53f9295ac81a7d5f
PR1ME <sanya_fl@ukr.net>
2017-06-28 21:57:20 +0300

    letter

--- views/letter/letter.php
+++ views/letter/letter.php
@@ -3,14 +3,14 @@ use yii\helpers\Html;
 use app\models\Sent;
 ?>
 <div class="message-show">
-    <table class="table-message-view">
+    <table class="table table-message-view">
 <?php
 
 foreach ($letter as $let) {
-    echo '<tr><td>' . $let['date_dep'] . '</td></tr>';
-    echo '<tr><td>Получатель:' . $let['recipient'] . '</td></tr>';
-    echo '<tr><td>Тема письма:' . $let['subject'] . '</td></tr>';
-    echo '<tr><td>' . $let['message'] . '</td></tr>';
+    echo '<tr><td><i>' . $let['date_dep'] . '</i></td></tr>';
+    echo '<tr><td><b>Получатель: </b><i>' . $let['recipient'] . '</i></td></tr>';
+    echo '<tr><td><b>Тема письма: </b><i>' . $let['subject'] . '</i></td></tr>';
+    echo '<tr><td><i>' . $let['message'] . '</i></td></tr>';
 }
 ?>
     </table>
