d589f1c81e36ea4a20b5f528a65e9982753a5abd
Telkov <sanya_fl@ukr.net>
2017-06-29 08:41:43 +0300

    update

--- views/site/index.php
+++ views/site/index.php
@@ -1,13 +1,19 @@
 <?php
+use yii\helpers\Html;
+use yii\helpers\Url;
 
 $this->title = 'My Yii Application';
+
+$url = Url::toRoute(['site/login']);
 ?>
+
 <div class="site-index">
     <div class="jumbotron">
         <h1>Привет!</h1>
 
         <p class="lead">Тестовое задание реализовано с помощью фреймфорка Yii 2.0</p>
 
-        <p><a class="btn btn-lg btn-success" href="http://itmaster/web/index.php?r=site/login">Вперед</a></p>
+        <!-- <p><a class="btn btn-lg btn-success" href="http://itmaster/web/index.php?r=site/login">Вперед</a></p> -->
+        <p><a class="btn btn-lg btn-success" href= <?= $url ?> >Вперед</a></p>
     </div>
 </div>
