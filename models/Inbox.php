53b5e25b6cc1da90b22a5113cdc2367169bae6ff
PR1ME <sanya_fl@ukr.net>
2017-07-01 23:55:44 +0300

    temp

--- models/Inbox.php
+++ models/Inbox.php
@@ -17,10 +17,18 @@ class Inbox extends Model
         echo "You have $numEmails in your mailbox <br>";
         for ($i = 1; $i < $numEmails + 1; $i++) {
             $mailHeader = imap_headerinfo($inbox, $i);
-            $from = $mailHeader->fromaddress;
-            $subject = strip_tags($mailHeader->subject);
-            $date = $mailHeader->date;
-            $body = imap_body($inbox, $i);
+            $from = iconv_mime_decode($mailHeader->fromaddress, 0, "UTF-8");  //Перекодируем в нужный формат
+            $subject = iconv_mime_decode(strip_tags($mailHeader->subject), 0, "UTF-8");   // Перекодируем в нужный формат
+            $date = date('d.m.Y H:i', strtotime($mailHeader->date));  //Перекодируем в нужный формат
+            $body = imap_body($inbox, $i, 2);
+            if($part->encoding == 3) {
+                $body = imap_base64($body);
+            } else if($part->encoding == 1) {
+                $body = imap_8bit($body);
+            } else {
+                $body = imap_qprint($body);
+            }
+            debug($body);
             $allmails[] = array('id' => $i, 'from' => $from, 'subject' => $subject, 'date' => $date, 'body' => $body);
         }
 
