42325e4788196cc049ece8a275ffe360a41b7e61
PR1ME <sanya_fl@ukr.net>
2017-07-03 01:16:35 +0300

    в процессе переделки

---
+++ components/FormWidget.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Alejandro
+ * Date: 03.07.2017
+ * Time: 0:38
+ */
+
+namespace app\components;
+
+
+class FormWidget
+{
+
+}
---
+++ components/views/form.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Alejandro
+ * Date: 03.07.2017
+ * Time: 0:42
+ */
