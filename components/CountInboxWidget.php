773e2b5af36e174ed06acd33de55385f69cff3a5
PR1ME <sanya_fl@ukr.net>
2017-07-03 22:28:08 +0300

    temp

---
+++ components/CountInboxWidget.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Alejandro
+ * Date: 03.07.2017
+ * Time: 21:47
+ */
+
+namespace app\components;
+
+
+class CountInboxWidget
+{
+
+}
---
+++ components/InboxWidget.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Alejandro
+ * Date: 03.07.2017
+ * Time: 18:31
+ */
+
+namespace app\components;
+
+
+class InboxWidget
+{
+
+}
